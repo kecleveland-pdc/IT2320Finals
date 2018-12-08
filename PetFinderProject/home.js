a7a7ad39b1e57496406d84d68ed8fac10d3b404f
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-12-07 19:37:00 -0500

    Added search by shelter functionality.

--- PetFinderProject/home.js
+++ PetFinderProject/home.js
@@ -1,53 +1,9 @@
 
 $(function(){
 //jQuery code goes here
-    $("#btn1").on("click", function(){
-        var url = "https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=?&tags="
-                    +$("#photosearch").val();   
-       $.ajax({
-           url: url,           
-           type: "GET", 
-           dataType: "jsonp",   //needed for CORS       
-           contentType: "application/json; charset=utf-8",
-           crossDomain: true,                   
-           success: function(data){
-              console.log(data);             
-              $.each(data.items, function(i, item){                
-                var img = $("<img></img>");
-                img.attr("src",item.media.m);
-                $("#flikrresults").append(img);
-              });              
-           }
-         });
-    });
-
-    $("#btn2").on("click", function(){
-        const key = "551b527add03634ac3a95a7a967367e6";           
-        var animal = $("#breedsearch").val();
-        var url = "http://api.petfinder.com/breed.list?format=json&key="+key+"&callback=?&animal="+animal+"&format=json";
-       $.ajax({
-           url: url,           
-           type: "GET", 
-           dataType: "jsonp",   //needed for CORS       
-           contentType: "application/json; charset=utf-8",
-           crossDomain: true,                   
-           success: function(data){              
-              console.log(data);
-              $("ul").remove();
-              var ul = $("<ul>Breeds</ul>");
-              $("#breedresults").append(ul);
-              
-              $.each(data.petfinder.breeds.breed,function(i,breed){                  
-                    var li = $("<li></li>").text(breed.$t);
-                    $("ul").append(li);
-              });                      
-            }                     
-
-         });//ajax  
-    }); //btn click
 
     //test
-    $("#submit").click(function(e){
+    $("#submit-SeePets").click(function(e){
         e.preventDefault();
 
         const key = "7ca41b0ec396887de2e110e7a3c60b80"; 
@@ -81,4 +37,32 @@ $(function(){
             }   
          });//ajax  
     });  //btn click
-})
+
+    $("#submit-GetPetsShelter").click(function(e){
+        e.preventDefault();
+        const key = "7ca41b0ec396887de2e110e7a3c60b80"; 
+        var location = $("#zipcode").val();
+        var sheltername = "";
+        var url = "http://api.petfinder.com/shelter.find?callback=?&format=json&key="+key+"&callback=?&location="+location+"&sheltername="+sheltername+"&_=&format=json";
+        console.log(url);
+       $.ajax({
+           url: url,           
+           type: "GET", 
+           dataType: "jsonp",      
+           contentType: "application/json; charset=utf-8",
+           crossDomain: true,                 
+           success: function(data){              
+              var div = $("<div id='result-div'><div>")
+              $.each(data.petfinder.shelters.shelter,function(i,shelter){ 
+                var petdiv = $("<div id='petdiv' class='shadow'></div>")
+                var shelterName = $("<p></p>").append("<strong>Shelter Name: </strong>" + shelter.name.$t);
+                var shelterID =  $("<p id = " + shelter.id.$t + "></p>").append("<strong>ID:</strong> " + shelter.id.$t);
+                $(petdiv).append(shelterName);
+                $(petdiv).append(shelterID);
+                $(div).append(petdiv);
+              });
+              $("#results").html(div);          
+            }   
+         });//ajax  
+    });  //btn click
+});//end jquery
